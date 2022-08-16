bbcf7bd0de4be05f2ac1f65f82974b3e943429e5
Ema <emafaillace2016@outlook.com>
2022-08-16 14:35:52 -0300

    Se añadio el metodo eliminarAlumno a la clase Universidad

--- Universidad.py
+++ Universidad.py
@@ -9,3 +9,8 @@ class Universidad:
     def listarAlumnos(self):
         for alumno in self.alumnos:
             print(alumno)
+    
+    def eliminarAlumno(self, legajo):
+        for alumno in self.alumnos:
+            if alumno.getLegajo() == legajo:
+                self.alumnos.remove(alumno)
